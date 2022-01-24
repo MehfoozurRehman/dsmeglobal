ab5300e52e1aa48449b71cb861b6d56ac6266427
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-01-24 17:40:48 +0500

    header and home and intro section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,96 @@
 import React from "react";
+import Header from "./components/Header";
 import "./styles/App.scss";
+import "animate.css";
+import { Route, Routes, Link } from "react-router-dom";
+import video from "./assets/video.mp4";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+function CarouselEntry({ subHeading, heading, buttonLabel, toPath, isRight }) {
+  return (
+    <div className="home__section__carousel__entry">
+      <video src={video} autoPlay={"autoplay"} muted={true} loop={true} />
+      <div className="home__section__carousel__entry__overlay">
+        <div
+          className={
+            isRight
+              ? "home__section__carousel__entry__overlay__content home__section__carousel__entry__overlay__content__right"
+              : "home__section__carousel__entry__overlay__content"
+          }
+        >
+          <div className="home__section__carousel__entry__overlay__content__sub__heading">
+            We Find
+          </div>
+          <div className="home__section__carousel__entry__overlay__content__heading">
+            OUR UNIQUE WAY
+          </div>
+          <Link to={toPath ? toPath : "/"} className="button">
+            {buttonLabel}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Carousel
+                autoPlay={true}
+                interval={4000}
+                showArrows={false}
+                showStatus={false}
+                infiniteLoop={true}
+              >
+                <CarouselEntry
+                  subHeading="WE FIND"
+                  heading="OUR UNIQUE WAY"
+                  buttonLabel="View Our Work"
+                  toPath="/"
+                />
+                <CarouselEntry
+                  subHeading="WE THINK"
+                  heading="OUT OF THE BOX"
+                  isRight={true}
+                  buttonLabel="View Our Work"
+                  toPath="/"
+                />
+                <CarouselEntry
+                  subHeading="WE PREFER"
+                  heading="You Over Anything"
+                  buttonLabel="View Our Work"
+                  toPath="/"
+                />
+              </Carousel>
+              <div className="into__section">
+                <div className="into__section__wrapper">
+                  <div className="into__section__heading">
+                    WE DELIVER <span>SOLUTIONS</span>
+                  </div>
+                  <video
+                    src={video}
+                    className="into__section__video"
+                    autoPlay={"autoplay"}
+                    muted={true}
+                    loop={true}
+                  />
+                  <Link to="/" className="button">
+                    View All Services
+                  </Link>
+                </div>
+              </div>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </>
+  );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { X } from "react-feather";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+function Nav() {
+  return (
+    <div className="nav animate__animated animate__fadeInDown">
+      <Link to="/" className="nav__link">
+        Home
+      </Link>
+      <Link to="about_us" className="nav__link">
+        About Us
+      </Link>
+      <Link to="/services" className="nav__link">
+        Services
+      </Link>
+      <Link to="/portfolio" className="nav__link">
+        Portfolio
+      </Link>
+      <Link to="/contact_us" className="nav__link">
+        Contact Us
+      </Link>
+    </div>
+  );
+}
+
+export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  return (
+    <div className="header">
+      <div className="header__wrapper">
+        <a href="#" className="brand__name">
+          <img src={logo} alt="logo" className="brand__name__logo" />
+        </a>
+        <button
+          className="nav__menu__btn"
+          onClick={() => {
+            isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true);
+          }}
+        >
+          {isNavOpen ? (
+            <X color="currentColor" size={30} strokeWidth={3} />
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="29.221"
+              height="22.522"
+              viewBox="0 0 29.221 22.522"
+            >
+              <g
+                id="Group_5"
+                data-name="Group 5"
+                transform="translate(-1773.279 -41.5)"
+              >
+                <line
+                  id="Line_1"
+                  data-name="Line 1"
+                  x2="25.221"
+                  transform="translate(1775.279 43.5)"
+                  fill="none"
+                  stroke="#fff"
+                  stroke-linecap="round"
+                  stroke-width="4"
+                />
+                <line
+                  id="Line_2"
+                  data-name="Line 2"
+                  x2="11.869"
+                  transform="translate(1788.631 52.761)"
+                  fill="none"
+                  stroke="#fff"
+                  stroke-linecap="round"
+                  stroke-width="4"
+                />
+                <line
+                  id="Line_3"
+                  data-name="Line 3"
+                  x2="22.254"
+                  transform="translate(1778.246 62.022)"
+                  fill="none"
+                  stroke="#fff"
+                  stroke-linecap="round"
+                  stroke-width="4"
+                />
+              </g>
+            </svg>
+          )}
+        </button>
+        {isNavOpen ? <Nav /> : null}
+      </div>
+    </div>
+  );
+}
