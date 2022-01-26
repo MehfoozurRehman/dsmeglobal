2c9ba9f81d4b1215a49f2e76beb9fce99daa2d49
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-01-26 14:38:58 +0500

    footer compnt

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Home from "./screens/Home";
 import "./styles/App.scss";
+import "./styles/footer.scss";
 import "animate.css";
 import Header from "./components/Header";
 import Services from "./screens/Services";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -15,6 +17,7 @@ export default function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/services" element={<Services />}></Route>
       </Routes>
+      <Footer />
     </>
   );
 }
