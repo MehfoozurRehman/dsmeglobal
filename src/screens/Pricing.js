ddc4677917f88d41b1f9745a4caddcad87ccc4bd
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-02-01 16:23:17 +0500

    services section partlially done

--- src/screens/Pricing.js
+++ src/screens/Pricing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomeSectionCarouselEntry from "../components/HomeSectionCarouselEntry";
+import deliveryPic from "../assets/delivery.png";
 function PricingFeatureEntry() {
   return (
     <div className="pricing__feature__entry">
@@ -11,6 +12,31 @@ function PricingFeatureEntry() {
     </div>
   );
 }
+const DeliveryCardEntry1 = [
+  { para: "Onsite / Offsite Discovery Workshop" },
+  { para: "Vision and Goal Mapping" },
+  { para: "Stakeholders Interviews" },
+  { para: "Requirements Definition" },
+  { para: "UI/UX and Visual Prototyping" },
+  { para: "High Risk Tech POCs" },
+  { para: "Product Roadmap" },
+];
+const DeliveryCardEntry2 = [
+  { para: " Software Architecture and Design" },
+  { para: "Development of Web / Mobile / Backend Components" },
+  { para: "APIs Development and Integrations" },
+  { para: "Continuous Integration/Delivery" },
+  { para: "UAQ" },
+  { para: "QA Testing" },
+];
+const DeliveryCardEntry3 = [
+  { para: "SLA Based Support" },
+  { para: "L3 and Production Support" },
+  { para: "Services" },
+  { para: "Operational support" },
+  { para: "On-going Support" },
+];
+
 export default function Pricing() {
   return (
     <>
@@ -31,6 +57,50 @@ export default function Pricing() {
         <PricingFeatureEntry />
         <PricingFeatureEntry />
         <PricingFeatureEntry />
+      </div>
+      <div className="pricing__delivery__section">
+        <div className="pricing__delivery__section__content__wrapper">
+          <div className="pricing__delivery__section__heading">
+            Process <span>From Idea Creation to Product Delivery</span>
+          </div>
+          <img
+            src={deliveryPic}
+            alt="deliveryPic"
+            className="pricing__delivery__section__img"
+          />
+          <div className="pricing__delivery__section__content">
+            <div className="pricing__delivery__section__content__card">
+              <div className="pricing__delivery__section__content__card__heading">
+                Discovery
+              </div>
+              {DeliveryCardEntry1.map((item) => (
+                <div className="pricing__delivery__section__content__card__para">
+                  {item.para}
+                </div>
+              ))}
+            </div>
+            <div className="pricing__delivery__section__content__card">
+              <div className="pricing__delivery__section__content__card__heading">
+                Execution
+              </div>
+              {DeliveryCardEntry2.map((item) => (
+                <div className="pricing__delivery__section__content__card__para">
+                  {item.para}
+                </div>
+              ))}
+            </div>
+            <div className="pricing__delivery__section__content__card">
+              <div className="pricing__delivery__section__content__card__heading">
+                Support
+              </div>
+              {DeliveryCardEntry3.map((item) => (
+                <div className="pricing__delivery__section__content__card__para">
+                  {item.para}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
