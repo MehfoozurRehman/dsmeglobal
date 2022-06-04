2f13a305e4b756d27db4a5a2bd9c63b6ccb4fdb5
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-06-04 13:49:26 +0500

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,122 +1,104 @@
-import React, { useState } from "react";
-import { X } from "react-feather";
-import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import React, { useLayoutEffect, useState } from "react";
+import { X, Menu } from "react-feather";
+import { Link, useNavigate } from "react-router-dom";
 import logoDark from "../assets/logoDark.png";
-import Nav from "./Nav";
 
-export default function Header({ isDark }) {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+export default function Header() {
+  const navigate = useNavigate();
+  const [isNavOpen, setIsNavOpen] = useState(true);
+
+  function navOpenClose() {
+    if (window.innerWidth < 1000) {
+      setIsNavOpen(false);
+    } else {
+      setIsNavOpen(true);
+    }
+  }
+  useLayoutEffect(() => {
+    navOpenClose();
+    window.addEventListener("resize", navOpenClose);
+  }, []);
+
   return (
     <div className="header">
-      <div className="header__wrapper">
-        <Link to="/" className="brand__name">
-          {isDark ? (
-            <img src={logoDark} alt="logo" className="brand__name__logo" />
-          ) : (
-            <img src={logo} alt="logo" className="brand__name__logo" />
-          )}
+      <div className="header__content">
+        <Link to="/" className="header__content__logo">
+          <img src={logoDark} alt="logo" />
         </Link>
-        <button
-          className="nav__menu__btn"
-          onClick={() => {
-            isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true);
-          }}
-        >
-          {isNavOpen ? (
-            <X color="currentColor" size={30} strokeWidth={3} />
-          ) : isDark ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="29.221"
-              height="22.522"
-              viewBox="0 0 29.221 22.522"
-            >
-              <g
-                id="Group_5"
-                data-name="Group 5"
-                transform="translate(-1773.279 -41.5)"
-              >
-                <line
-                  id="Line_1"
-                  data-name="Line 1"
-                  x2="25.221"
-                  transform="translate(1775.279 43.5)"
-                  fill="none"
-                  stroke="#242424"
-                  strokeLinecap="round"
-                  strokeWidth="4"
-                />
-                <line
-                  id="Line_2"
-                  data-name="Line 2"
-                  x2="11.869"
-                  transform="translate(1788.631 52.761)"
-                  fill="none"
-                  stroke="#242424"
-                  strokeLinecap="round"
-                  strokeWidth="4"
-                />
-                <line
-                  id="Line_3"
-                  data-name="Line 3"
-                  x2="22.254"
-                  transform="translate(1778.246 62.022)"
-                  fill="none"
-                  stroke="#242424"
-                  strokeLinecap="round"
-                  strokeWidth="4"
-                />
-              </g>
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="29.221"
-              height="22.522"
-              viewBox="0 0 29.221 22.522"
-            >
-              <g
-                id="Group_5"
-                data-name="Group 5"
-                transform="translate(-1773.279 -41.5)"
-              >
-                <line
-                  id="Line_1"
-                  data-name="Line 1"
-                  x2="25.221"
-                  transform="translate(1775.279 43.5)"
-                  fill="none"
-                  stroke="#fff"
-                  strokeLinecap="round"
-                  strokeWidth="4"
-                />
-                <line
-                  id="Line_2"
-                  data-name="Line 2"
-                  x2="11.869"
-                  transform="translate(1788.631 52.761)"
-                  fill="none"
-                  stroke="#fff"
-                  strokeLinecap="round"
-                  strokeWidth="4"
-                />
-                <line
-                  id="Line_3"
-                  data-name="Line 3"
-                  x2="22.254"
-                  transform="translate(1778.246 62.022)"
-                  fill="none"
-                  stroke="#fff"
-                  strokeLinecap="round"
-                  strokeWidth="4"
-                />
-              </g>
-            </svg>
-          )}
-        </button>
-        {isNavOpen ? <Nav setIsNavOpen={setIsNavOpen} /> : null}
+        {isNavOpen ? <Nav navigate={navigate} /> : null}
+        <div className="header__content__button">
+          <button
+            className="header__content__button__secondary"
+            onClick={() => {
+              setIsNavOpen(!isNavOpen);
+            }}
+          >
+            {isNavOpen ? (
+              <X size={20} color="currentColor" />
+            ) : (
+              <Menu size={20} color="currentColor" />
+            )}
+          </button>
+          <Link
+            to="/contact-us"
+            onClick={() => {
+              document
+                .getElementsByName("header__content__nav__entry")
+                .forEach((item) => {
+                  item.checked = false;
+                });
+            }}
+            className="header__content__button__primary"
+          >
+            Let's Talk
+          </Link>
+        </div>
       </div>
+    </div>
+  );
+}
+
+function NavLink({ children, to, navigate }) {
+  return (
+    <div className="header__content__nav__entry">
+      <input
+        type="radio"
+        name="header__content__nav__entry"
+        className="header__content__nav__entry__input"
+        defaultChecked={window.location.pathname === to}
+        onClick={() => {
+          navigate(to);
+        }}
+      />
+      <div className="header__content__nav__entry__content">{children}</div>
+    </div>
+  );
+}
+
+function Nav({ navigate }) {
+  return (
+    <div className="header__content__nav">
+      <NavLink to="/" navigate={navigate}>
+        Home
+      </NavLink>
+      <NavLink to="/about-us" navigate={navigate}>
+        About Us
+      </NavLink>
+      <NavLink to="/services" navigate={navigate}>
+        Services
+      </NavLink>
+      <NavLink to="/portfolio" navigate={navigate}>
+        Portfolio
+      </NavLink>
+      <NavLink to="/pricing" navigate={navigate}>
+        Pricing
+      </NavLink>
+      <NavLink to="/careers" navigate={navigate}>
+        Careers
+      </NavLink>
+      <NavLink to="/blog" navigate={navigate}>
+        Blog
+      </NavLink>
     </div>
   );
 }
