09a981f97d1dc7f55c88a5a994d84e506927c1a1
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-06-27 16:53:58 +0500

    dsme icons done

--- src/components/EcommerceWorkCodeCard.js
+++ src/components/EcommerceWorkCodeCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Box } from "react-feather";
 
-export default function EcommerceWorkCodeCard({ label, info }) {
+export default function EcommerceWorkCodeCard({ svg, label, info }) {
   return (
     <div className="ecommerce__screen__container__services__content__entry">
-      <Box size={20} color="currentColor" strokeWidth={1.5} />
+      {svg}
       <div className="ecommerce__screen__container__services__content__entry__heading">
         {label}
       </div>
