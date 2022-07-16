a6ecc7b285bc4cbb27275449219ce8765f26506b
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2022-07-16 18:51:08 +0500

    Mobile Application Development change  done

--- src/screens/ServiceMobDev.js
+++ src/screens/ServiceMobDev.js
@@ -459,6 +459,288 @@ export default function ServicesMobDev() {
             </div>
           ))}
       </div>
+      <div className="how__we__do__section">
+        <div className="how__we__do__section__heading">How We Do It</div>
+        <div className="how__we__do__section__subheading">
+          Fast-track your project's delivery using our simple three-step
+          application development process.
+        </div>
+        <div className="how__we__do__section__cards">
+          <div className="how__we__do__section__card">
+            <div className="how__we__do__section__card__heading">
+              1. Requirements Gathering
+            </div>
+            <div className="how__we__do__section__card__detail">
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Requirements Definition
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Analyzing the requirements
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Documenting the process
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Provide initial design
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="how__we__do__section__card">
+            <div className="how__we__do__section__card__heading">
+              2. Development & Testing
+            </div>
+            <div className="how__we__do__section__card__detail">
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Frontend & backend development
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Weekly Client Feedback (SCRUM)
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  QA Testing
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Deployment
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="how__we__do__section__card">
+            <div className="how__we__do__section__card__heading">
+              3. Support & Maintenance
+            </div>
+            <div className="how__we__do__section__card__detail">
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  SLA Based Support
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  L3 and Production Support Services
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  Operational support where needed
+                </div>
+              </div>
+              <div className="how__we__do__section__card__detail__line">
+                <div className="how__we__do__section__card__detail__line__svg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-check-circle">
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                </div>
+                <div className="how__we__do__section__card__detail__line__text">
+                  On-going Support
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container__feature">
         <div className="container__feature__col">
           <div className="container__feature__col__heading">
