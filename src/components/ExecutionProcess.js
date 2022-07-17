6e9783d96af3ec1141588bbe26c0755e5eb70837
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-17 15:20:11 +0500

    ServiceUIUX cleanup

---
+++ src/components/ExecutionProcess.js
@@ -0,0 +1,317 @@
+import React from "react";
+
+export function ExecutionProcess({}) {
+  return (
+    <div className="june_second_main_container">
+      <div className="june_second_main_all_container">
+        <div className="june_third_container">
+          <div className="june_third_container_title">Execution Process</div>
+          <div className="june_third_container_para">
+            We work with you to make appealing sites, impactful applications,
+            and modern enterprise systems individuals love. Through your vision
+            and our expertise, our team of business analysts, designers, and
+            developers will build experiences that keep your customers coming
+            back.
+          </div>
+        </div>
+        <div className="june_third_container_first_box">
+          <div className="june_third_container_first_box__colomns">
+            <div className="june_third_container_first_box__colomns__heading">
+              <div className="june_third_container_first_box__colomns__heading__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  width="40"
+                  height="40"
+                  className="feather feather-file-text"
+                >
+                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                  <polyline points="14 2 14 8 20 8"></polyline>
+                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                  <polyline points="10 9 9 9 8 9"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__heading__text">
+                Discovery
+              </div>
+            </div>
+
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Understand business pain points
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Interviews & observations
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Functional requirements
+              </div>
+            </div>
+          </div>
+          <div className="june_third_container_first_box__colomns">
+            <div className="june_third_container_first_box__colomns__heading">
+              <div className="june_third_container_first_box__colomns__heading__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-box"
+                >
+                  <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
+                  <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+                  <line x1="12" y1="22.08" x2="12" y2="12"></line>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__heading__text">
+                Wireframes
+              </div>
+            </div>
+
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Concept sketching
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                System/Process flow
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Information architecture
+              </div>
+            </div>
+          </div>
+          <div className="june_third_container_first_box__colomns">
+            <div className="june_third_container_first_box__colomns__heading">
+              <div className="june_third_container_first_box__colomns__heading__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-layout"
+                >
+                  <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="3" y1="9" x2="21" y2="9"></line>
+                  <line x1="9" y1="21" x2="9" y2="9"></line>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__heading__text">
+                Build & Iterate
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Interaction design
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                High-fidelity mockups
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                Interactive prototypes
+              </div>
+            </div>
+            <div className="june_third_container_first_box__colomns__contents">
+              <div className="june_third_container_first_box__colomns__contents__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-check-circle"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+              </div>
+              <div className="june_third_container_first_box__colomns__contents__text">
+                UI Style guide/brand identity
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
