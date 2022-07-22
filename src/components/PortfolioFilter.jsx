f4a9e46431e03e5a63ffe3a21c2be1d56da6edcc
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2022-07-22 22:54:15 +0500

    portfolio animation

--- src/components/PortfolioFilter.jsx
+++ src/components/PortfolioFilter.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React from 'react';
+import {Bounce} from 'react-reveal';
 
-export function PortfolioFilter({ title, onChange, defaultChecked }) {
+export function PortfolioFilter({title, onChange, defaultChecked}) {
   return (
-    <div className="blog__page__filter__entry">
-      <input
-        type="radio"
-        className="blog__page__filter__entry__input"
-        name="portfolio__page__filter__entry"
-        onChange={onChange}
-        defaultChecked={defaultChecked}
-      />
-      <div className="blog__page__filter__entry__content">{title}</div>
-    </div>
+    <Bounce right>
+      <div className="blog__page__filter__entry">
+        <input
+          type="radio"
+          className="blog__page__filter__entry__input"
+          name="portfolio__page__filter__entry"
+          onChange={onChange}
+          defaultChecked={defaultChecked}
+        />
+        <div className="blog__page__filter__entry__content">{title}</div>
+      </div>
+    </Bounce>
   );
 }
--- src/components/ProjectCardContent.jsx
+++ src/components/ProjectCardContent.jsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React from 'react';
+import {Bounce, Zoom} from 'react-reveal';
 
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from 'react-router-dom';
 
-export default function ProjectCardContent({ image, title, data }) {
+export default function ProjectCardContent({image, title, data}) {
   const navigate = useNavigate();
 
   return (
-    <button
-      className="service__details__projects__entry"
-      onClick={() => {
-        navigate("/portfolio-detail");
-        window.localStorage.setItem("project", JSON.stringify(data));
-        window.scrollTo({ behavior: "smooth", top: 0 });
-      }}
-    >
-      <img
-        loading="lazy"
-        src={
-          "https://res.cloudinary.com/mehfoozurrehman/image/upload/q_50/" +
-          image
-        }
-        alt="projectImg"
-        className="service__details__projects__entry__img"
-      />
-      <div className="service__details__projects__entry__content">
-        <div className="service__details__projects__entry__content__heading">
-          {title}
+    <Zoom>
+      <button
+        className="service__details__projects__entry"
+        onClick={() => {
+          navigate('/portfolio-detail');
+          window.localStorage.setItem('project', JSON.stringify(data));
+          window.scrollTo({behavior: 'smooth', top: 0});
+        }}>
+        <img
+          loading="lazy"
+          src={
+            'https://res.cloudinary.com/mehfoozurrehman/image/upload/q_50/' +
+            image
+          }
+          alt="projectImg"
+          className="service__details__projects__entry__img"
+        />
+        <div className="service__details__projects__entry__content">
+          <div className="service__details__projects__entry__content__heading">
+            {title}
+          </div>
+          <div className="service__details__projects__entry__content__info">
+            {data.description}
+          </div>
         </div>
-        <div className="service__details__projects__entry__content__info">
-          {data.description}
-        </div>
-      </div>
-    </button>
+      </button>
+    </Zoom>
   );
 }
