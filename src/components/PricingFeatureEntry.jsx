406d6869b21c79f0b8c5a08bf600c004310f1caa
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2022-07-22 22:49:17 +0500

    industry animation

--- src/components/PricingFeatureEntry.jsx
+++ src/components/PricingFeatureEntry.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React from 'react';
+import {Bounce} from 'react-reveal';
 
-export function PricingFeatureEntry({ title, content, icon }) {
+export function PricingFeatureEntry({title, content, icon}) {
   return (
-    <div className="pricing__feature__entry">
-      {icon ? (
-        <div className="pricing__feature__entry__icon">{icon}</div>
-      ) : null}
-      <div className="pricing__feature__entry__heading">{title}</div>
-      <div className="pricing__feature__entry__info">{content}</div>
-    </div>
+    <Bounce right>
+      <div className="pricing__feature__entry">
+        {icon ? (
+          <div className="pricing__feature__entry__icon">{icon}</div>
+        ) : null}
+        <div className="pricing__feature__entry__heading">{title}</div>
+        <div className="pricing__feature__entry__info">{content}</div>
+      </div>
+    </Bounce>
   );
 }
--- src/screens/Industry.jsx
+++ src/screens/Industry.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { HomeJumbotron } from "../components/HomeJumbotron";
-import { IndustryFeatrues } from "../components/IndustryFeatrues";
-import industries from "../assets/industry.webp";
+import React from 'react';
+import {HomeJumbotron} from '../components/HomeJumbotron';
+import {IndustryFeatrues} from '../components/IndustryFeatrues';
+import industries from '../assets/industry.webp';
+import {Bounce} from 'react-reveal';
 
 export default function Industry() {
   return (
@@ -14,17 +15,23 @@ export default function Industry() {
           talk={true}
         />
       </div>
-      <div className="industry__software__solutions">
-        Software solutions for industry leaders
-      </div>
-      <div className="industry__software__solutions__content">
-        Need a software solution that’s right for you? Choose a digital product
-        crafted to answer the challenges of your industry. Small or big,
-        startup, or enterprise – we partner with companies of various sizes and
-        profiles on their way to innovation and growth. Empower your business
-        with a state-of-the-art digital solution, tailored to your needs and
-        expectations.
-      </div>
+      <Bounce left>
+        {' '}
+        <div className="industry__software__solutions">
+          Software solutions for industry leaders
+        </div>
+      </Bounce>
+      <Bounce up>
+        {' '}
+        <div className="industry__software__solutions__content">
+          Need a software solution that’s right for you? Choose a digital
+          product crafted to answer the challenges of your industry. Small or
+          big, startup, or enterprise – we partner with companies of various
+          sizes and profiles on their way to innovation and growth. Empower your
+          business with a state-of-the-art digital solution, tailored to your
+          needs and expectations.
+        </div>
+      </Bounce>
       <IndustryFeatrues />
     </>
   );
