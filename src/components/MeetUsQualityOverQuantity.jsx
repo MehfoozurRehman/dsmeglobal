3192486b1c64e045276eaea9f22a41ad4929560c
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2022-07-22 19:53:17 +0500

    meet us Animations

--- src/components/MeetUsQualityOverQuantity.jsx
+++ src/components/MeetUsQualityOverQuantity.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import qualityOver from "../assets/qualityOver.svg";
-import continuousChange from "../assets/continuousChange.svg";
-import fouceOn from "../assets/fouceOn.svg";
+import React from 'react';
+import qualityOver from '../assets/qualityOver.svg';
+import continuousChange from '../assets/continuousChange.svg';
+import fouceOn from '../assets/fouceOn.svg';
+import {Bounce} from 'react-reveal';
 
 export function MeetUsQualityOverQuantity() {
   return (
@@ -9,67 +10,80 @@ export function MeetUsQualityOverQuantity() {
       <div className="meetus__container__sub">
         <div className="meetus__container__sub__content">
           <div className="meetus__container__sub__left">
-            <div className="meetus__container__sub__left__image">
-              <img
-                loading="lazy"
-                src={qualityOver}
-                alt="Quality over quantity"
-              />
-            </div>
+            <Bounce left>
+              <div className="meetus__container__sub__left__image">
+                <img
+                  loading="lazy"
+                  src={qualityOver}
+                  alt="Quality over quantity"
+                />
+              </div>
+            </Bounce>
           </div>
-          <div className="meetus__container__sub__right">
-            <div className="meetus__container__sub__right__heading">
-              <span>Quality </span> over quantity
+          <Bounce right>
+            <div className="meetus__container__sub__right">
+              <div className="meetus__container__sub__right__heading">
+                <span>Quality </span> over quantity
+              </div>
+
+              <div className="meetus__container__sub__right__para">
+                When it comes to our job, it's quality that matters. UX
+                research, code reviews, manual and automatic tests. No
+                exceptions.
+              </div>
             </div>
-            <div className="meetus__container__sub__right__para">
-              When it comes to our job, it's quality that matters. UX research,
-              code reviews, manual and automatic tests. No exceptions.
-            </div>
-          </div>
+          </Bounce>
         </div>
       </div>
       <div className="meetus__container__sub">
         <div
           className="meetus__container__sub__content"
-          id="meetus__container__sub__content2"
-        >
-          <div className="meetus__container__sub__left">
-            <div className="meetus__container__sub__right__heading">
-              Continuous <span>change</span>
+          id="meetus__container__sub__content2">
+          <Bounce left>
+            <div className="meetus__container__sub__left">
+              <div className="meetus__container__sub__right__heading">
+                Continuous <span>change</span>
+              </div>
+              <div className="meetus__container__sub__right__para">
+                Change is the only constant. We believe in that. Technology
+                switch? Sure thing! New frameworks and tools? More than welcome.
+              </div>
             </div>
-            <div className="meetus__container__sub__right__para">
-              Change is the only constant. We believe in that. Technology
-              switch? Sure thing! New frameworks and tools? More than welcome.
-            </div>
-          </div>
+          </Bounce>
           <div className="meetus__container__sub__right">
-            <div className="meetus__container__sub__left__image">
-              <img
-                loading="lazy"
-                src={continuousChange}
-                alt="Continuous change"
-              />
-            </div>
+            <Bounce right>
+              <div className="meetus__container__sub__left__image">
+                <img
+                  loading="lazy"
+                  src={continuousChange}
+                  alt="Continuous change"
+                />
+              </div>
+            </Bounce>
           </div>
         </div>
       </div>
       <div className="meetus__container__sub">
         <div className="meetus__container__sub__content">
           <div className="meetus__container__sub__left">
-            <div className="meetus__container__sub__left__image">
-              <img loading="lazy" src={fouceOn} alt="Fouce on people" />
-            </div>
+            <Bounce left>
+              <div className="meetus__container__sub__left__image">
+                <img loading="lazy" src={fouceOn} alt="Fouce on people" />
+              </div>
+            </Bounce>
           </div>
-          <div className="meetus__container__sub__right">
-            <div className="meetus__container__sub__right__heading">
-              Focus on <span>people</span>
+          <Bounce right>
+            <div className="meetus__container__sub__right">
+              <div className="meetus__container__sub__right__heading">
+                Focus on <span>people</span>
+              </div>
+              <div className="meetus__container__sub__right__para">
+                “People matter”. Such a cliché, right? But at DSME Global Links,
+                they really do. Family atmosphere and tons of laughs guaranteed.
+                See for yourself.
+              </div>
             </div>
-            <div className="meetus__container__sub__right__para">
-              “People matter”. Such a cliché, right? But at DSME Global Links,
-              they really do. Family atmosphere and tons of laughs guaranteed.
-              See for yourself.
-            </div>
-          </div>
+          </Bounce>
         </div>
       </div>
     </div>
--- src/components/MeetUsSmallThingsBigDifferences.jsx
+++ src/components/MeetUsSmallThingsBigDifferences.jsx
@@ -1,27 +1,34 @@
-import React from "react";
-import agilefirst from "../assets/agileFirst.svg";
+import React from 'react';
+import agilefirst from '../assets/agileFirst.svg';
+import {Bounce, Zoom} from 'react-reveal';
 
 export function MeetUsSmallThingsBigDifferences() {
   return (
     <div className="meetus__container__smallThings">
       <div className="meetus__container__smallThings__content">
-        <div className="meetus__container__smallThings__heading">
-          Small things. <span>Big difference</span>
-        </div>
-        <div className="meetus__container__smallThings__content__container">
-          <div className="meetus__container__smallThings__left">
-            <div className="meetus__container__smallThings__left__heading">
-              <span>Agile </span> first
-            </div>
-            <div className="meetus__container__smallThings__left__para">
-              For some people, Agile is dull. For us, it is the way to adapt,
-              deliver and grow.
-            </div>
+        <Zoom In>
+          <div className="meetus__container__smallThings__heading">
+            Small things. <span>Big difference</span>
           </div>
-          <div className="meetus__container__smallThings__right">
-            <div className="meetus__container__smallThings__right__image">
-              <img loading="lazy" src={agilefirst} alt="Agile first" />
+        </Zoom>
+        <div className="meetus__container__smallThings__content__container">
+          <Bounce left>
+            <div className="meetus__container__smallThings__left">
+              <div className="meetus__container__smallThings__left__heading">
+                <span>Agile </span> first
+              </div>
+              <div className="meetus__container__smallThings__left__para">
+                For some people, Agile is dull. For us, it is the way to adapt,
+                deliver and grow.
+              </div>
             </div>
+          </Bounce>
+          <div className="meetus__container__smallThings__right">
+            <Bounce right>
+              <div className="meetus__container__smallThings__right__image">
+                <img loading="lazy" src={agilefirst} alt="Agile first" />
+              </div>
+            </Bounce>
           </div>
         </div>
       </div>
