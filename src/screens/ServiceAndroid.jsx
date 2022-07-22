1024cc30c26785323216a6d1bd99b2d6df2285f4
ahmadhaqofficial <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-07-22 08:01:02 -0700

    Android change

--- src/screens/ServiceAndroid.jsx
+++ src/screens/ServiceAndroid.jsx
@@ -258,27 +258,12 @@ export default function ServicesAndroid() {
         </div>
         <div className="ios__development__tangible">
           <div className="ios__development__tangible__col__one__heading">
-            Tangible results, right on schedule
+            Tangible results, <span>right on schedule</span>
           </div>
           <div className="ios__development__tangible__col__two">
-            <div className="ios__development__tangible__col__two__header">
-              <span className="ios__development__tangible__col__two__header__weeks">
-                2 weeks
-              </span>{" "}
-              for the prototype
-            </div>
-            <div className="ios__development__tangible__col__two__header__middle">
-              Need your idea verified fast?
-            </div>
             <button className="ios__development__tangible__col__two__header__middle__button">
               Get a quote
             </button>
-          </div>
-          <div className="ios__development__tangible__col__three__header">
-            <span className="ios__development__tangible__col__three__header__weeks">
-              3 weeks
-            </span>{" "}
-            for the MVP
           </div>
         </div>
         <div className="ios__development__native__ios">
